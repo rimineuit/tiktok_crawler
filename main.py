a43f8ff9d8b26d739154e10d1814799c5326d6bc
rimineuit <minh0974680144@gmail.com>
2025-10-10 10:01:04 +0700

    Làm sạch bằng cách đưa vào module từng folder và sử dụng thư viện asyncio để chạy

--- main.py
+++ main.py
@@ -7,11 +7,27 @@ import json
 import os
 import sys
 
-# Tính đường dẫn tuyệt đối của file hiện tại
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print(BASE_DIR)
+
 #Tạo FastAPI app
-app = FastAPI()
+app = FastAPI(
+    title="Crawler",
+    description="""
+    API nội bộ để crawl video và metadata từ TikTok.
+    
+    **Chức năng chính:**
+    - Crawl video & metadata
+    - Crawl comment
+    - Lưu JSON kết quả
+    
+    **Lưu ý:** Chỉ dành cho mục đích nghiên cứu.
+    """,
+    version="1.0.0",
+    contact={
+        "name": "RIMINE",
+        "email": "minh0974680144@gmail.com",
+    },
+)
+
 env = os.environ.copy()
 
 # Đảm bảo UTF-8 cho subprocess
@@ -26,34 +42,27 @@ class TikTokBody(BaseModel):
     max_items: int = 30
     max_comments: int = 100
 
-@app.post("/tiktok/get_video_links_and_metadata")
+@app.post("/tiktok/get_video_links_and_metadata", tags=["TikTok Crawler"], summary="Lấy danh sách video trên trang cá nhân")
 async def tiktok_get_video_links_and_metadata(body: TikTokBody):
     try:
-        # Gọi crawler trực tiếp bằng await
         label = body.label.strip().lower()
         browser_type = body.browser_type.strip().lower()
         max_comments = str(body.max_comments)
-        # Nối các URL thành một chuỗi cách nhau bởi dấu cách
         clean_url = body.url.strip()
         max_items = str(body.max_items).strip()
         script_path = 'tiktok.get_list_videos'
         cmd = [sys.executable, "-m" ,script_path, browser_type, label, max_items, clean_url, max_comments]
-        print(*cmd)
-        try:
-            proc = await asyncio.create_subprocess_exec(
-                *cmd,
-                env=env,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE
-            )
-            stdout_b, stderr_b = await proc.communicate()
-            out = stdout_b.decode("utf-8", "ignore")
-            err = stderr_b.decode("utf-8", "ignore")
-
-            if proc.returncode != 0:
-                raise HTTPException(500, detail=f"Script exit {proc.returncode}:\n{err}")
-        except subprocess.TimeoutExpired:
-            raise HTTPException(status_code=504, detail="⏱️ Quá thời gian xử lý")
+        
+        # Gọi subprocess
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            env=env,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+        stdout_b, stderr_b = await proc.communicate()
+        out = stdout_b.decode("utf-8", "ignore")
+        err = stderr_b.decode("utf-8", "ignore")
             
         try:
             # Lấy phần output sau chữ "Result"
@@ -61,7 +70,7 @@ async def tiktok_get_video_links_and_metadata(body: TikTokBody):
             if result_start == -1:
                 raise ValueError("Không tìm thấy đoạn 'Result' trong stdout")
 
-            json_part = proc.stdout[result_start:]  # phần sau "Result"
+            json_part = out[result_start:]
             # Tìm JSON mảng đầu tiên bắt đầu bằng [ và kết thúc bằng ]
             json_match = re.search(r"\[\s*{[\s\S]*?}\s*\]", json_part)
             
